86657bd1325bbbcde6dc7aecf0f1e6f4062ede21
trflorian <florian.trautweiler@hispeed.ch>
2025-04-23 20:56:26 +0200

    add pydantic ai, dotenv, env examples and implement basic agent retry loop

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
-def main():
-    print("Hello from pydantic-ai-retry-fallback!")
+import asyncio
+from pydantic_ai import Agent
+from pydantic_ai.settings import ModelSettings
+
+from dotenv import load_dotenv
+
+
+async def main() -> None:
+    load_dotenv()
+
+    agent = Agent(
+        model_settings=ModelSettings(timeout=5.0),
+    )
+
+    # MODEL
+    model_name = ""
+    prompt = "Who is Rick Astley?"
+
+    # RETRY
+    num_retries = 5
+
+    for retry in range(num_retries):
+        try:
+            response = agent.run_sync(
+                model=model_name,
+                user_prompt=prompt,
+            )
+            if response:
+                break
+
+        except Exception as e:
+            print(f"Error invoking API (retry {retry}/{num_retries}): {e}")
+    else:
+        # if the for loop doesn't break, all retries have failed
+        raise ValueError(f"API call unsuccessful after {num_retries} attempts")
+
+    print(f"API call successful: {response}")
 
 
 if __name__ == "__main__":
-    main()
+
+    asyncio.run(main())
